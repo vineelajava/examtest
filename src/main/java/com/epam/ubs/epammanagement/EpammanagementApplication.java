ae1562b7e60361f46a4440d4ad8a0488fca4dd1b
vineelamiriyala <vineela.miriyala@jmlsoftware.co.uk>
2022-01-31 18:24:30 +0000

    Initializing first commits

---
+++ src/main/java/com/epam/ubs/epammanagement/EpammanagementApplication.java
@@ -0,0 +1,13 @@
+package com.epam.ubs.epammanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EpammanagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EpammanagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/epam/ubs/epammanagement/EpammanagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.epam.ubs.epammanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EpammanagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
